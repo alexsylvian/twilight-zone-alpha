f25a1a3152f8edc5d0942d3812e0465e22956106
alexsylvian <lazeratus1@gmail.com>
2023-08-30 22:09:09 -0400

    added separate function, added images

--- twilight.js
+++ twilight.js
@@ -1,16 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const twilightContainer = document.getElementById('episode-container')
+    const twilightList= document.getElementById('cards')
 
     function renderEpisodeList(){
         fetch('http://localhost:3000/twilightZone')
         .then(res => res.json())
         .then(episodeList => {
             episodeList.forEach(episode => {
-                const episodeCard = document.createElement('p')
-                episodeCard.textContent = episode.title
-                twilightContainer.appendChild(episodeCard)
+                renderEpisode(episode)
+                // const episodeCard = document.createElement('div')
+                // episodeCard.textContent = episode.title
+                // twilightContainer.appendChild(episodeCard)
             })
         })
+    }
+
+    function renderEpisode(episode){
+        let episodeCard = document.createElement('li')
+        episodeCard.className = "episode-card"
+        episodeCard.textContent = episode.title
+        let episodeCardImage = document.createElement('img')
+        episodeCardImage.src = episode.image_url
+        episodeCard.appendChild(episodeCardImage)
+        cards.appendChild(episodeCard)
     }
 
     renderEpisodeList()
